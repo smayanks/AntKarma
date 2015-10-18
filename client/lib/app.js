888e7031a2294ff336a1797f06e2e40219e8fc37
saurabh0908 <saurabhshrivastava09@gmail.com>
2015-10-19 02:34:38 +0530

    Initial commit

---
+++ client/lib/app.js
@@ -0,0 +1,22 @@
+// if (Meteor.isClient) {
+
+	angular.module('antkarma', ['angular-meteor', 'ui.router']);
+
+// 	angular.module('antkarma').config(function($urlRouterProvider, $stateProvider, $locationProvider) {
+
+// 		$locationProvider.html5Mode(true);
+// 		$stateProvider
+// 			.state('questionnaire', {
+// 				url: '/questionnaire',
+// 				templateUrl: 'templates/questionnaire.ng.html',
+// 				controller: 'questionnaireCtrl'
+// 			});
+
+// 			$urlRouterProvider.otherwise('/');
+// 	});
+
+// 	angular.module('antkarma').controller('questionnaireCtrl', function($scope, $meteor) {
+
+// 	});
+
+// }
