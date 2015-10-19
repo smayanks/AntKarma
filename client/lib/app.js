d44dc7d7c15b8217702523318f1a1369300bd90f
saurabh0908 <saurabhshrivastava09@gmail.com>
2015-10-19 11:08:30 +0530

    modal widow

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,22 +1,24 @@
-// if (Meteor.isClient) {
+if (Meteor.isClient) {
 
 	angular.module('antkarma', ['angular-meteor', 'ui.router']);
 
-// 	angular.module('antkarma').config(function($urlRouterProvider, $stateProvider, $locationProvider) {
+	angular.module('antkarma').config(function($urlRouterProvider, $stateProvider, $locationProvider) {
 
-// 		$locationProvider.html5Mode(true);
-// 		$stateProvider
-// 			.state('questionnaire', {
-// 				url: '/questionnaire',
-// 				templateUrl: 'templates/questionnaire.ng.html',
-// 				controller: 'questionnaireCtrl'
-// 			});
+		// $locationProvider.html5Mode(true);
+		$stateProvider
+			.state('questionnaire', {
+				url: '/questionnaire',
+				templateUrl: '../templates/questionnaire.ng.html',
+				controller: 'questionnaireCtrl'
+			});
 
-// 			$urlRouterProvider.otherwise('/');
-// 	});
+			$urlRouterProvider.otherwise('/questionnaire');
+	});
 
-// 	angular.module('antkarma').controller('questionnaireCtrl', function($scope, $meteor) {
+	angular.module('antkarma').controller('questionnaireCtrl', function($scope, $meteor) {
 
-// 	});
 
-// }
+	});
+
+}
+
