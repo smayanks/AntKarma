147176dacce2a6e09eb65a6dd73e50c67ad338d5
saurabh0908 <saurabhshrivastava09@gmail.com>
2015-10-19 18:13:55 +0530

    Added fade in for 2nd level questions

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -1,19 +1,23 @@
-if (Meteor.isClient) {
 
-	angular.module('antkarma', ['angular-meteor', 'ui.router',  'ui.bootstrap']);
+	angular.module('antkarma', ['angular-meteor', 'ngAnimate','ui.router',  'ui.bootstrap']);
 
-	angular.module('antkarma').config(function($urlRouterProvider, $stateProvider, $locationProvider) {
+	// angular.module('antkarma').config(function($urlRouterProvider, $stateProvider, $locationProvider) {
 
-		// $locationProvider.html5Mode(true);
-		$stateProvider
-			.state('questionnaire', {
-				url: '/questionnaire',
-				templateUrl: 'client/templates/questions.ng.html'
-				
-			});
-			// $urlRouterProvider.otherwise('/questionnaire');
-		});
+	// 	// $locationProvider.html5Mode(true);
+	// 	$stateProvider
+	// 	.state('questionnaire', {
+	// 		url: '/questionnaire',
+	// 		templateUrl: 'client/templates/questions.ng.html'
+			
+	// 	});
 
-}
+	// 		// $urlRouterProvider.otherwise('/questionnaire');
+	// 	});
+
+	angular.module('antkarma').controller('QuestionsCtrl', function($scope, $modal, $meteor) {
+
+	});
+
+
 
 
